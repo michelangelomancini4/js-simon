f6ffe5481e6b66e94335e449a3d3e5bda681d625
michelangelomancini <michelangelomancini8@gmail.com>
2025-01-14 16:32:27 +0100

    optimizing random numbers creation

--- js/script.js
+++ js/script.js
@@ -1,42 +1,17 @@
-// Visualizzare in pagina 5 numeri casuali. 
-// creazione numeri random
-// 1
-let numberRandom1 = document.getElementById('numbers-list1');
-numberRandom1 = Math.floor(Math.random() *50)+ 1;
+// creazione numero random
+let numberRandom = document.getElementById('numbers-list');
+numberRandom = Math.floor(Math.random() *50)+ 1;
 
-// aggiunta numero random alla pagina
-const listaNumeri = document.getElementById('numbers-list1');
-listaNumeri.innerText = numberRandom1;
+// visualizzazione in console del numero random
+console.log(numberRandom);
 
-// 2
-let numberRandom2 = document.getElementById('numbers-list2');
-numberRandom2 = Math.floor(Math.random() *50)+ 1;
+const listaNumeri = document.getElementById('numbers-list');
 
-// aggiunta numero random alla pagina
-const listaNumeri2 = document.getElementById('numbers-list2');
-listaNumeri2.innerText = numberRandom2;
+const randomNumbers = Array.from({ length: 5 }, () => Math.floor(Math.random() * 50) + 1);
+console.log(randomNumbers);
 
-let numberRandom3= document.getElementById('numbers-list3');
-numberRandom3= Math.floor(Math.random() *50)+ 1;
-
-// aggiunta numero random alla pagina
-const listaNumeri3 = document.getElementById('numbers-list3');
-listaNumeri3.innerText = numberRandom3;
-// 4
-let numberRandom4 = document.getElementById('numbers-list4');
-numberRandom4 = Math.floor(Math.random() *50)+ 1;
-
-// aggiunta numero random alla pagina
-const listaNumeri4 = document.getElementById('numbers-list4');
-listaNumeri4.innerText = numberRandom4;
-// 5
-let numberRandom5 = document.getElementById('numbers-list5');
-numberRandom5 = Math.floor(Math.random() *50)+ 1;
-
-// aggiunta numero random alla pagina
-const listaNumeri5 = document.getElementById('numbers-list5');
-listaNumeri5.innerText = numberRandom5;
-
+// visualizzazione in pagina
+listaNumeri.innerText = randomNumbers;
 
 // Da lì parte un timer di 30 secondi. 
 setTimeout (myFunction, 5000);
@@ -45,6 +20,7 @@ function myFunction(){
 }
 
 // Dopo 30 secondi i numeri scompaiono e appaiono invece 5 input in cui l’utente deve inserire i numeri che ha visto precedentemente, nell’ordine che preferisce.
+
 
 
 
